b657a9447a64a5fbe2140740094224dc9a8727ae
PredictiveManish <manisht0914@gmail.com>
2025-08-03 17:51:03 +0530

    day-1

--- Ch-2 Data Preprocessing/aggregation.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-data = {'Date':['2021-01-01','2021-01-01','2021-01-02','2021-01-02'],
-        'Sales':[1000,1200,800,900]}
-df = pd.DataFrame(data)
-print(f"Original Dataframe: {df}")
-aggregated_df = df.groupby('Date')['Sales'].sum().reset_index()
-print("\nAggregatedDataFrame (Sum og Sales By Date):")
-print(aggregated_df)
--- Ch-2 Data Preprocessing/log_transformation.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-data = np.array([1,2,5,10,20,50,100])
-transformed_data = np.log(data)
-
-print(f"Original Data: {data}")
-
-print(f"\nTransformed Data (Log Transformation): {transformed_data}")
